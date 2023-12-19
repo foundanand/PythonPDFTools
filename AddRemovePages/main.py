c366a4faf9b0701f33727f3e1a568da7fe902df7
foundanand <anand.lahoti2021@vitbhopal.ac.in>
2023-12-19 17:21:21 +0400

    IC

--- AddRemovePages/main.py
+++ AddRemovePages/main.py
@@ -1,7 +1,5 @@
 import PyPDF2
 
-input_pdf = ""
-output_pdf = ""
 
 
 def insert_page(input_pdf, output_pdf, page_to_insert, insert_index):
@@ -26,22 +24,40 @@ def insert_page(input_pdf, output_pdf, page_to_insert, insert_index):
         with open(output_pdf, 'wb') as output_file:
             pdf_writer.write(output_file)
 
-def remove_page(input_pdf, output_pdf, page_to_remove):
-    with open(input_pdf, 'rb') as file:
-        pdf_reader = PyPDF2.PdfFileReader(file)
-        pdf_writer = PyPDF2.PdfFileWriter()
+def remove_page(input_path, output_path, page_number):
+    # Open the input PDF file
+    with open(input_path, 'rb') as file:
+        # Create a PDF reader object
+        pdf_reader = PyPDF2.PdfReader(file)
 
-        for i in range(pdf_reader.numPages):
-            if i + 1 != page_to_remove:
-                pdf_writer.addPage(pdf_reader.getPage(i))
+        # Create a PDF writer object
+        pdf_writer = PyPDF2.PdfWriter()
 
-        # Write the result to the output file
-        with open(output_pdf, 'wb') as output_file:
+        # Add all pages to the writer except the specified page number
+        for current_page_number in range(len(pdf_reader.pages)):
+            if current_page_number + 1 != page_number:
+                page = pdf_reader.pages[current_page_number]
+                pdf_writer.add_page(page)
+
+        # Open the output PDF file and write the modified content
+        with open(output_path, 'wb') as output_file:
             pdf_writer.write(output_file)
 
 # Example usage:
 # Replace 'input.pdf', 'output.pdf', 'new_page.pdf' with your actual file names
-insert_page('input.pdf', 'output.pdf', 'new_page.pdf', insert_index=2)
+# insert_page('input.pdf', 'output.pdf', 'new_page.pdf', insert_index=2)
+
+
+input_pdf = "/Users/anand/Developer/Python/PythonPDFTools/AddRemovePages/GAISOHB.pdf"
+output_pdf_inserted = "/Users/anand/Developer/Python/PythonPDFTools/AddRemovePages/GAISOHB_modified.pdf"  # Update the output file path after insertion
+output_pdf_removed = "/Users/anand/Developer/Python/PythonPDFTools/AddRemovePages/GAISOHB_removed.pdf"  # Specify a new output file path for the final result
+
+# Replace 'new_page.pdf' with the actual file name you want to insert
+new_page = "AddRemovePages/new_page.pdf"
 
 # Replace 'input.pdf' and 'output.pdf' with your actual file names
-remove_page('input.pdf', 'output.pdf', page_to_remove=3)
+# insert_page(input_pdf, output_pdf, new_page, insert_index=2)
+
+# Replace 'input.pdf' and 'output.pdf' with your actual file names
+remove_page(input_pdf, output_pdf_removed,3)
+
